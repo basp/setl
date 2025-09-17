440ccdc4f14621ef20f334abd8d083202b4f6688
Bas Pennings <baspster@gmail.com>
2025-09-18 01:50:00 +0200

    Better example.

--- src/Setl.Cmd/TextSerializer2Example.cs
+++ src/Setl.Cmd/TextSerializer2Example.cs
@@ -5,81 +5,101 @@ namespace Setl.Cmd;
 
 internal static partial class TextSerializer2Example
 {
-    private static readonly ITextSerializer berSerializer = 
-        new TextSerializer2Builder()
-            .Field(f =>
-            {
-                f.Name = "Recordcode";
-                f.Length = 4;
-                f.Converter = new TrimConverter();
-            })
-            .Field(f =>
-            {
-                f.Name = "Berichttype";
-                f.Length = 3;
-                f.Converter = new TrimConverter();
-            })
-            .Build();
+    private static class SVBWWBTextSerializer
+    {
+        private static readonly ITextSerializer berSerializer = 
+            new TextSerializer2Builder()
+                .Field(f =>
+                {
+                    f.Name = "Recordcode";
+                    f.Length = 4;
+                    f.Converter = new TrimConverter();
+                })
+                .Field(f =>
+                {
+                    f.Name = "Berichttype";
+                    f.Length = 3;
+                    f.Converter = new TrimConverter();
+                })
+                .Build();
     
-    private static readonly ITextSerializer gemSerializer =
-        new TextSerializer2Builder()
-            .Field(f =>
-            {
-                f.Name = "Recordcode";
-                f.Length = 4;
-                f.Converter = new TrimConverter();
-            })
-            .Field(f =>
-            {
-                f.Name = "Gemeentecode";
-                f.Length = 4;
-                f.Converter = new PadLeftConverter(4, '0');
-            })
-            .Build();
+        private static readonly ITextSerializer gemSerializer =
+            new TextSerializer2Builder()
+                .Field(f =>
+                {
+                    f.Name = "Recordcode";
+                    f.Length = 4;
+                    f.Converter = new TrimConverter();
+                })
+                .Field(f =>
+                {
+                    f.Name = "Gemeentecode";
+                    f.Length = 4;
+                    f.Converter = new PadLeftConverter(4, '0');
+                })
+                .Build();
     
-    private static readonly ITextSerializer dtrSerializer =
-        new TextSerializer2Builder()
-            .Field(f =>
-            {
-                f.Name = "Recordcode";
-                f.Length = 4;
-                f.Converter = new TrimConverter();
-            })
-            .Field(f =>
-            {
-                f.Name = "SofinummerHp";
-                f.Length = 9;
-                f.Converter = new PadLeftConverter(9, '0');
-            })
-            .Build();
+        private static readonly ITextSerializer dtrSerializer =
+            new TextSerializer2Builder()
+                .Field(f =>
+                {
+                    f.Name = "Recordcode";
+                    f.Length = 4;
+                    f.Converter = new TrimConverter();
+                })
+                .Field(f =>
+                {
+                    f.Name = "SofinummerHp";
+                    f.Length = 9;
+                    f.Converter = new PadLeftConverter(9, '0');
+                })
+                .Build();
     
-    private static readonly ITextSerializer tpgSerializer =
-        new TextSerializer2Builder()
-            .Field(f =>
+        private static readonly ITextSerializer tpgSerializer =
+            new TextSerializer2Builder()
+                .Field(f =>
+                {
+                    f.Name = "Recordcode";
+                    f.Length = 4;
+                    f.Converter = new TrimConverter();
+                })
+                .Field(f =>
+                {
+                    f.Name = "TotaalAantalGerechtigden";
+                    f.Length = 11;
+                    f.Converter = new Int32Converter();
+                })
+                .Build();
+        
+        private static readonly Dictionary<Regex, ITextSerializer> serializers = 
+            new()
             {
-                f.Name = "Recordcode";
-                f.Length = 4;
-                f.Converter = new TrimConverter();
-            })
-            .Field(f =>
+                [TextSerializer2Example.BerRegex()] = berSerializer,
+                [TextSerializer2Example.GemRegex()] = gemSerializer,
+                [TextSerializer2Example.DtrRegex()] = dtrSerializer,
+                [TextSerializer2Example.TpgRegex()] = tpgSerializer,
+            };
+
+        public static Row Parse(string line)
+        {
+            foreach (var serializer in serializers)
             {
-                f.Name = "TotaalAantalGerechtigden";
-                f.Length = 11;
-                f.Converter = new Int32Converter();
-            })
-            .Build();
+                if (!serializer.Key.IsMatch(line))
+                {
+                    continue;
+                }
+             
+                return serializer.Value.Deserialize(line);
+            }
+
+            var msg = $"No serializer found for '{line}'.";
+            throw new Exception(msg);
+        }
+    }
     
     public static void Run()
     {
         const string path = @"D:\temp\SVB\SVBWWB65PLUS00002_3.txt";
-        var serializers = new Dictionary<Regex, ITextSerializer>
-        {
-            [TextSerializer2Example.BerRegex()] = TextSerializer2Example.berSerializer,
-            [TextSerializer2Example.GemRegex()] = TextSerializer2Example.gemSerializer,
-            [TextSerializer2Example.DtrRegex()] = TextSerializer2Example.dtrSerializer,
-            [TextSerializer2Example.TpgRegex()] = TextSerializer2Example.tpgSerializer,
-        };
-
         using var stream = File.OpenRead(path);
         using var reader = new StreamReader(stream);
         while (!reader.EndOfStream)
@@ -90,17 +110,9 @@ internal static partial class TextSerializer2Example
                 continue;
             }
 
-            foreach (var serializer in serializers)
-            {
-                if (!serializer.Key.IsMatch(line))
-                {
-                    continue;
-                }
-             
-                var row = serializer.Value.Deserialize(line);
-                var json = row.ToJson();
-                Console.WriteLine(json);
-            }
+            var row = SVBWWBTextSerializer.Parse(line);
+            var json = row.ToJson();
+            Console.WriteLine(json);
         }
     }
 
